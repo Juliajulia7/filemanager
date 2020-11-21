7a25e9a116b0f696cbb024495caae1a524f52dee
Juliajulia7 <kjulia2777@gmail.com>
2020-11-21 12:31:28 +0400

    -upload, download, delete in one method

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from flask import Flask, render_template, request,  send_from_directory
 
 
 def hashFor(data):
-    # Prepare the project id hash
     hashId = hashlib.md5()
     hashId.update(repr(data).encode('utf-8'))
     return hashId.hexdigest()
@@ -15,7 +14,10 @@ def get(url):
     return requests.get(url)
 
 def pathFile(hashname):
-    dir1 = hashname[0:2]  # ??
+    if len(hashname) >= 2:
+       dir1 = hashname[0:2]
+    else:
+        hashname=hashname + '0z'
     # pathdir='/storage/'+'dir1' + '/' #./
     pathdir = 'C:/Users/yuliya.kudasova/PycharmProjects/test' + '/storage/' + dir1 + '/'
     path = pathdir + hashname
@@ -25,41 +27,37 @@ def pathFile(hashname):
 
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 @app.route("/")
 def index():
     return render_template("mainpage.html")
 
-@app.route("/upload", methods=['POST'])
+@app.route("/managerfile", methods=['POST', 'GET', 'DELETE'])
 def upload():
-    for file in request.files.getlist("file"):
+    if request.method == 'POST':
+        file =request.files['file']
         hashfile = hashFor(file)
         filename= pathFile(hashfile)
-        print(filename)
         file.save(filename)
-    return render_template("complete.html", hashfile=hashfile)
-
-@app.route('/download', methods=['POST'])
-def download_file():
-    if request.method == 'POST':
-        filename = request.form.get('namefile')
-        pathdir = 'C:/Users/yuliya.kudasova/PycharmProjects/test/storage/' + filename[0:2]
-    return send_from_directory(pathdir, filename, as_attachment=True)
-
-@app.route('/delete', methods=['POST'])
-def delete_file():
-    if request.method == 'POST':
-        filename = request.form.get('deletefile')
-        print(filename)
+        return hashfile
+    elif request.method == 'GET':
+        filename = request.form.get('filename')
+        if len(filename)>=2:
+            pathdir = 'C:/Users/yuliya.kudasova/PycharmProjects/test/storage/' + filename[0:2]
+            return send_from_directory(pathdir, filename, as_attachment=True)
+        else:
+            return "Name is not correct"
+    elif request.method == 'DELETE':
+        filename = request.form.get('filename')
         path = pathFile(filename)
-        print(path)
-        try:
+        if os.path.exists(path):
             os.remove(path)
-        except OSError as e:
-            pass
-    return render_template("mainpage.html")
+            return "Ok"
+        else:
+            return "File does not exist"
+
 
 if __name__ == "__main__":
     app.run()
